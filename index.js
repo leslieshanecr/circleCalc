c625df4b9bbe8a63fca1ed78d9a61eeecd8cc68b
leslieshanecr <leslier7@hawaii.edu>
2020-05-12 23:42:25 +0000

    add input for cylinder height, calculate & display volume

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 // create an app objndoe ect from the express object
 const app = express();
 // this allows the parsing of the html file using body parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //this sends the html file to the web page using the root directory
 app.get("/", function(req, res) {
@@ -13,17 +13,24 @@ app.get("/", function(req, res) {
 });
 
 // this gets the response from the values in the web page
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
-  var radCircle = parseFloat(req.body.n1);;
+  var radCircle = parseFloat(req.body.n1);
 
-// does the computation of the input variables, as numbers
-    var areaCircle = Math.PI * Math.pow(radCircle, 2);
-    var circumCircle = 2 * Math.PI * radCircle;
+  // does the computation of the input variables, as numbers
+  var areaCircle = Math.PI * Math.pow(radCircle, 2);
+  var circumCircle = 2 * Math.PI * radCircle;
 
-// sends the results back to the web page as string
-  res.send("The Area of the Cirle is " + areaCircle.toFixed(2) + " and the Circumference is " + circumCircle.toFixed(2));
-})
+
+  var cylinderHeight = parseFloat(req.body.n2);
+
+  //computes the volume of cyclinder
+  var volume = Math.PI * Math.pow(radCircle, 2) * cylinderHeight;
+
+
+  // sends the results back to the web page as string
+  res.send("The Area of the Circle: " + areaCircle.toFixed(2) + " Circumference: " + circumCircle.toFixed(2) + " Volume: " + volume.toFixed(2));
+});
 //this gets the response from the web page to this placeholder
 // COMMENT out this code in Repl, as this is not running on localhost:3000
 
